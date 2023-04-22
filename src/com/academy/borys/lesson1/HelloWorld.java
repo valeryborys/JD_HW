475f04a2fa89c121ad90b50066db00b7d26f2734
Valery Borys <valeryborys@gmail.com>
2023-04-22 10:08:07 +0200

    added new file and comment in file examples

--- src/com/academy/borys/lesson1/HelloWorld.java
+++ src/com/academy/borys/lesson1/HelloWorld.java
@@ -2,6 +2,6 @@ package com.academy.borys.lesson1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!"); //деление на 0
     }
 }
